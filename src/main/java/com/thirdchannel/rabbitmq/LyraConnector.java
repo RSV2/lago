62d08869ce49c795d2289327bdf47784d495256d
Andrew Croswell <andrew@thirdchannel.com>
2017-07-11 07:01:49 -0400

    Fixed IN-438.  Added in recovery policy which wasn't hugely aggressive.  Also changes the general settings for retry continue indefinitely.

--- src/main/java/com/thirdchannel/rabbitmq/LyraConnector.java
+++ src/main/java/com/thirdchannel/rabbitmq/LyraConnector.java
@@ -3,10 +3,7 @@ package com.thirdchannel.rabbitmq;
 import com.rabbitmq.client.ConnectionFactory;
 import com.thirdchannel.rabbitmq.exceptions.RabbitMQSetupException;
 import net.jodah.lyra.Connections;
-import net.jodah.lyra.config.Config;
-import net.jodah.lyra.config.ConfigurableConnection;
-import net.jodah.lyra.config.RecoveryPolicies;
-import net.jodah.lyra.config.RetryPolicy;
+import net.jodah.lyra.config.*;
 import net.jodah.lyra.util.Duration;
 
 import java.io.IOException;
@@ -14,16 +11,16 @@ import java.util.concurrent.TimeoutException;
 
 public class LyraConnector {
 
-    private static final int MAX_RECONNECTION_ATTEMPTS = 20;
-    private static final Duration RABBITMQ_RECONNECTION_INTERVAL = Duration.seconds(1);
-    private static final Duration MAX_RABBITMQ_RECONNECTION_INTERVAL = Duration.minutes(5);
+    private static final Duration RABBITMQ_RECOVERY_INTERVAL = Duration.milliseconds(100);
+    private static final Duration RABBITMQ_MAX_RECOVERY_INTERVAL = Duration.seconds(10);
+    private static final Duration RABBITMQ_RETRY_INTERVAL = Duration.milliseconds(100);
+    private static final Duration RABBITMQ_MAX_RETRY_INTERVAL = Duration.seconds(10);
 
     private static final Config config = new Config()
-        .withRecoveryPolicy(RecoveryPolicies.recoverAlways())
+        .withRecoveryPolicy(new RecoveryPolicy()
+            .withBackoff(RABBITMQ_RECOVERY_INTERVAL, RABBITMQ_MAX_RECOVERY_INTERVAL))
         .withRetryPolicy(new RetryPolicy()
-            .withMaxAttempts(MAX_RECONNECTION_ATTEMPTS)
-            .withInterval(RABBITMQ_RECONNECTION_INTERVAL)
-            .withMaxDuration(MAX_RABBITMQ_RECONNECTION_INTERVAL)
+            .withBackoff(RABBITMQ_RETRY_INTERVAL, RABBITMQ_MAX_RETRY_INTERVAL)
         );
 
     public static ConfigurableConnection newConnection(ConnectionFactory factory) throws RabbitMQSetupException {
